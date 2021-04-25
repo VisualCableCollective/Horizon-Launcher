a6f55a44fd26c55ba11561d4753462ea01df94d6
joschiservice <joschua.hass.sh@gmail.com>
2021-04-25 13:57:24 +0200

    added basic title bar

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,14 +1,25 @@
 // Assets
 import XWhite from "../assets/x-white.png";
+import RestoreWindow from "../assets/restore-window-1.png";
 
 const TitleBar = () => {
+    let iconStyle = {height: "20px", widht: "auto"};
 
     return (
     <div className="title-bar flex items-start justify-end flex-row">
-        <div className="icons-wrapper" style={{marginTop: "5px", marginRight: "9px"}}>
-            <img src={XWhite} alt="X" style={{height:"20px", width: "auto"}} />
+        <div className="title-bar-icons-wrapper flex flex-row" style={{marginTop: "5px"}}>
+            <Icon icon={<img src={RestoreWindow} alt="Restore" style={iconStyle} />}/>
+            <Icon icon={<img src={XWhite} alt="Restore" style={iconStyle} />}/>
         </div>
     </div>
+    );
+}
+
+const Icon = ({icon}) => {
+    return (
+        <div className="title-bar-icon flex justify-center items-center mx-2">
+            {icon}
+        </div>
     );
 }
 
