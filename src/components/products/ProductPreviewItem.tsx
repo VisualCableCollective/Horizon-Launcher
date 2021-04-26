270f4903727f03b22c7c4697d3f60645ccd832bd
Joschua H <joschua.hass.sh@gmail.com>
2021-04-26 17:54:22 +0200

    added transition to banner-progress-indicator

--- src/components/products/ProductPreviewItem.tsx
+++ src/components/products/ProductPreviewItem.tsx
@@ -36,7 +36,7 @@ function ProductPreviewBanner({ productOwnershipStatus, bannerImgSrc, productIns
     return (
         <div className="product-preview-banner-wrapper" style={{ height: PRODUCT_PREVIEW_BANNER_HEIGHT+"px", width: PRODUCT_PREVIEW_BANNER_WIDTH+"px" }}>
             <div className="banner-hover-overly absolute z-20 rounded bg-white bg-opacity-0 hover:bg-opacity-10 transition-all duration-200 ease-in-out" style={{ height: PRODUCT_PREVIEW_BANNER_HEIGHT+"px", width: PRODUCT_PREVIEW_BANNER_WIDTH+"px" }} />
-            <div className="banner-progress-indicator absolute z-10 bg-left	bg-cover rounded" style={{ height: PRODUCT_PREVIEW_BANNER_HEIGHT+"px", width: progressIndicatorWidth+"px", backgroundImage: "url(" + bannerImgSrc + ")" }} />
+            <div className="banner-progress-indicator absolute z-10 bg-left	bg-cover rounded transition-all duration-500 ease-in-out" style={{ height: PRODUCT_PREVIEW_BANNER_HEIGHT+"px", width: progressIndicatorWidth+"px", backgroundImage: "url(" + bannerImgSrc + ")" }} />
             <div className="banner-bg absolute flex justify-center items-center" style={{ height: PRODUCT_PREVIEW_BANNER_HEIGHT+"px", width: PRODUCT_PREVIEW_BANNER_WIDTH+"px" }}>
                 <img src={bannerImgSrc} alt="Banner" className="rounded filter grayscale" style={{ height: PRODUCT_PREVIEW_BANNER_HEIGHT+"px", width: PRODUCT_PREVIEW_BANNER_WIDTH+"px" }} />
             </div>
