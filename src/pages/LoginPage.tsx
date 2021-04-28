51ceb981298bd2f1bf9d5088b6d81030dea4222e
joschiservice <joschua.hass.sh@gmail.com>
2021-04-28 17:56:13 +0200

    removed iframe stuff
    I will change the login strategy. Sign in with browser instead

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,10 @@
-import { useRef } from "react";
 import Config from "../Config";
 
 function LoginPage(){
     let loginURL = Config.getAPIServerURL();
-    let iFrame = useRef(null);
     return (
-        <div className="login-page">
-            <iframe ref={iFrame} title="Login" className="min-w-full min-h-screen" src={loginURL} onLoad={ () => {
-                
-            }} />
+        <div className="login-page bg-gray-900 min-h-screen min-w-full">
+            
         </div>
     );
 }
