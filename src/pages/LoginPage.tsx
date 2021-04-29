aac5ec265bd29ef75b2a071f308cb8f451f0ac46
joschiservice <joschua.hass.sh@gmail.com>
2021-04-29 15:13:46 +0200

    added bg-gradient-1

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import Config from "../Config";
 function LoginPage(){
     let loginURL = Config.getAPIServerURL();
     return (
-        <div className="login-page bg-gray-900 min-h-screen min-w-full">
+        <div className="login-page bg-gradient-1 min-h-screen min-w-full">
             
         </div>
     );
