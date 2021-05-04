c68b37844940f72eabe0bc4e2209e449efee718b
joschiservice <joschua.hass.sh@gmail.com>
2021-05-04 15:26:21 +0200

    improved window control icons

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,32 +1,35 @@
-// Assets
-import XWhite from "../assets/x-white.png";
-import RestoreWindow from "../assets/restore-window-1.png";
-
 // Modules
 import PropTypes, { InferProps } from "prop-types";
 
-const TitleBar = () => {
-    let iconStyle = {height: "20px", widht: "auto"};
+// Window Control SVG paths
+const closePath = 'M 0,0 0,0.7 4.3,5 0,9.3 0,10 0.7,10 5,5.7 9.3,10 10,10 10,9.3 5.7,5 10,0.7 10,0 9.3,0 5,4.3 0.7,0 Z';
+const restorePath = 'm 2,1e-5 0,2 -2,0 0,8 8,0 0,-2 2,0 0,-8 z m 1,1 6,0 0,6 -1,0 0,-5 -5,0 z m -2,2 6,0 0,6 -6,0 z';
+const maximizePath = 'M 0,0 0,10 10,10 10,0 Z M 1,1 9,1 9,9 1,9 Z';
+const minimizePath = 'M 0,5 10,5 10,6 0,6 Z';
 
+const TitleBar = () => {
     return (
     <div className="title-bar flex items-start justify-end flex-row">
         <div className="title-bar-icons-wrapper flex flex-row" style={{marginTop: "5px"}}>
-            <Icon icon={<img src={RestoreWindow} alt="Restore" style={iconStyle} />}/>
-            <Icon icon={<img src={XWhite} alt="Restore" style={iconStyle} />}/>
+            <Icon svgPath={minimizePath}/>
+            <Icon svgPath={maximizePath}/>
+            <Icon svgPath={closePath}/>
         </div>
     </div>
     );
 }
 
-function Icon({ icon }: InferProps<typeof Icon.propTypes>) {
+function Icon({ svgPath }: InferProps<typeof Icon.propTypes>) {
     return (
-        <div className="title-bar-icon flex justify-center items-center mx-2">
-            {icon}
+        <div className="title-bar-icon flex justify-center items-center mx-2" style={{width: "25px"}}>
+            <svg version="1.1" width="10" height="10" fill="currentColor">
+                <path d={svgPath} />
+            </svg>
         </div>
     );
 }
 Icon.propTypes = {
-    icon: PropTypes.element.isRequired
+    svgPath: PropTypes.string.isRequired
 }
 
 export default TitleBar;
