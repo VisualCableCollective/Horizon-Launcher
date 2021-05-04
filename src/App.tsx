c4a771cacddc9d9f56130798cb00f9db42e31e6f
joschiservice <joschua.hass.sh@gmail.com>
2021-05-04 15:26:29 +0200

    Revert "improved window control icons"
    This reverts commit c68b37844940f72eabe0bc4e2209e449efee718b.

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App font-titillium-web text-white">
       <LoadingOverlay isVisible={isLoadingOverlayVisible} />
-      <HomePage />
+      <LoginPage />
     </div>
   );
 }
