24575de653e247943b03f3bb5d8e2512895b7c12
joschiservice <joschua.hass.sh@gmail.com>
2021-05-09 12:59:53 +0200

    more design optimizations

--- react/src/pages/HomePage.tsx
+++ react/src/pages/HomePage.tsx
@@ -14,16 +14,16 @@ const HomePage = () => {
     dummyProduct.installationProgress = 80;
 
     return (
-        <div className="home-page">
+        <div className="home-page overflow-y-hidden h-screen">
             <TitleBar />
-            <div className="flex flex-col items-center px-4">
-                <div className="store-information-wrapper flex flex-col justify-center items-center max-w-screen-xl py-5 w-full mb-2.5">
+            <div className="flex flex-col items-center px-4 overflow-x-hidden overflow-y-scroll h-full">
+                <div className="store-information-wrapper flex flex-col justify-center items-center w-full max-w-screen-xl py-5 mb-2.5">
                     <h1 className="font-bold text-3xl text-center">Get ready for a new era.</h1>
                     <p className="mt-2">The Horizon storefront is currently under heavy developent. The Horizon storefront is currently under heavy development. You can find the latest information about our development progress on our website and on our Discord server. </p>
                 </div>
-                <div className="vcc-products-wrapper flex flex-col justify-center items-start w-full max-w-screen-xl">
+                <div className="vcc-products-wrapper flex-grow flex flex-col justify-center items-start max-w-screen-xl py-5">
                     <h2 className="text-lg font-semibold text-left w-full mb-2">Applications made by the VisualCableCollective</h2>
-                    <div className="apps-row-wrapper max-w-screen-xl flex flex-row items-start gap-3 overflow-x-scroll">
+                    <div className="apps-row-wrapper flex flex-wrap flex-row items-start gap-3 overflow-x-auto">
                         <ProductPreviewItem product={dummyProduct} />
                         <ProductPlaceholderPreviewItem />
                         <ProductPlaceholderPreviewItem />
