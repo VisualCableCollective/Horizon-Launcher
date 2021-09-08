61ae29e23bafafd23a20d58266598211e4f38d05
Joschua H <54814804+joschiservice@users.noreply.github.com>
2021-09-08 08:49:36 +0200

    init nextron project with TS and TailwindCS

--- src/renderer/next.config.js
+++
@@ -1,8 +0,0 @@
-const withImages = require('next-images')
-
-module.exports = withImages({
-  webpack: (config) => Object.assign(config, {
-    target: 'electron-renderer',
-  }),
-  fileExtensions: ["jpg", "jpeg", "png", "gif"],
-});
